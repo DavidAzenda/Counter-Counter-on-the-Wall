0b2b10a7962f3cd886556722f2f628c135df6f2b
DavidAzenda <51847798+DavidAzenda@users.noreply.github.com>
2022-01-11 13:21:47 -0700

    Add files via upload

---
+++ Counter Web Application/README.md
@@ -0,0 +1,5 @@
+# assignment-5-azenda
+Bemuter Azenda
+30003251
+Counter React App
+assignment-5-azenda created by GitHub Classroom
---
+++ Counter Web Application/reactcounter/src/App.js
@@ -0,0 +1,50 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import { useState} from 'react';
+
+
+function App(){
+  const [count,setCount] = useState(0);
+  const [text, setText] = useState(" ");
+
+  function getData(val){
+    setCount(val.target.value)
+    console.warn(val.target.value)
+  }
+  function increase(){
+    setCount(count + 1)
+  }
+
+  function decrease(){
+    setCount(count - 1)
+  };
+
+  function assign(){
+    setCount(parseInt(text))
+  };
+
+  return(
+      <header className = "App">
+        <div className = "App-grid-container">
+          <div className = "App-grid-item1">
+            <input type="text" placeholder = "Put a number" className = "App-input" onChange={e => setText(e.target.value)} />
+            <button  className = "App-button2" onClick = {assign}> Assign</button>
+
+          </div>
+          <div className = "App-grid-item2">
+            <h1 > {count} </h1>
+          </div>
+          <div className = "App-grid-item3">
+            <button className = "App-button4" onClick = {increase}>+</button>
+            <button className = "App-button3" onClick = {decrease}> - </button>
+          </div>
+        </div>
+
+      </header>
+
+
+  );
+
+}
+export default App;
